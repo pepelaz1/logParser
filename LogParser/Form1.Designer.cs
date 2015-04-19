9b9fd80e4b3eaba1d6cb70d307bb82ef3e28d726
Vladimir Baldetskiy <pepelaz@sibmail.com>
2015-04-19 18:37:11 +0600

    no message

---
+++ LogParser/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace LogParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time elapsed:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(96, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = " 0.00";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "secs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 85);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Log parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LogParser/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Net;
+
+namespace LogParser
+{
+    public partial class Form1 : Form
+    {
+        private String[] _files = { "googlem.txt", "google.txt", "msn.txt", "bing.txt" };
+        private DateTime _start_time = new DateTime();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessLine(String line, String filename)
+        {
+            Match m = Regex.Match(line, "([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3})", RegexOptions.None);
+            if (m.Groups.Count > 0)
+            {
+                using (StreamWriter sw = new StreamWriter(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\" + filename, true))
+                {
+                    sw.WriteLine(m.Groups[0].Value);
+                    sw.Close();
+                }
+            }
+        }
+
+        private void ProcessUrl(String url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Timeout = 30 * 60 * 1000;
+            request.UseDefaultCredentials = true;
+            request.Proxy.Credentials = request.Credentials; 
+            WebResponse response = (WebResponse)request.GetResponse();
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader log = new StreamReader(s))
+                {
+                    for (; ; )
+                    {
+                        if (log.EndOfStream)
+                            break;
+
+                        String line = log.ReadLine();
+                        if (line.Contains("Googlebot-Mobile"))
+                            ProcessLine(line, _files[0]);
+                        else if (line.Contains("Googlebot"))
+                            ProcessLine(line, _files[1]);
+                        else if (line.Contains("msnbot"))
+                            ProcessLine(line, _files[2]);
+                        else if (line.Contains("bingbot"))
+                            ProcessLine(line, _files[3]);
+                    }
+                    log.Close();
+                }
+            }
+        }
+
+        private void RemoveDups(String file)
+        {
+            List<String> IPs = new List<String>();  
+            using (StringReader reader = new StringReader(File.ReadAllText(file)))
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                    if (!IPs.Contains(line))
+                        IPs.Add(line);
+            }
+            using (StreamWriter writer = new StreamWriter(File.Open(file, FileMode.Create)))
+                foreach (string value in IPs)
+                    writer.WriteLine(value); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            button1.Enabled = false;
+            timer1.Enabled = true;
+            _start_time = DateTime.Now;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+           // foreach (String file in _files)
+          //      File.Delete(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\" + file);
+
+            String[] urls = File.ReadAllLines(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\input.txt");
+            foreach (String url in urls)
+                ProcessUrl(url);
+
+            foreach (String file in _files)
+                RemoveDups(file);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Cursor = Cursors.Default;
+            button1.Enabled = true;
+            timer1.Enabled = false;
+            MessageBox.Show("Ok");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = DateTime.Now - _start_time;
+            label2.Text = ts.TotalSeconds.ToString("0.00");
+        }
+
+    }
+}
